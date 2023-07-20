6bc2bc7ec8a20e8a5040ba9483ae2014f3c1de9f
Alex.Sn <alex7777@nxt.ru>
2023-07-20 15:53:59 +0300

    initial commit

---
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -0,0 +1,53 @@
+package ru.netology.delivery.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity() {
+        var cities = new String[]{
+                "Москва", "Санкт-Петербург", "Нижний Новгород", "Волгоград", "Ростов-на-Дону",
+                "Дмитров", "Самара", "Саратов", "Сочи", "Анапа",
+                "Тула", "Брянск", "Воронеж", "Курск", "Тверь"
+        };
+        return cities[new Random().nextInt(cities.length)];
+    }
+
+    public static String generateName(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        var faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
---
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -0,0 +1,52 @@
+package ru.netology.delivery.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+class DeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+//        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndReplanMeeting() {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+        $("[data-test-id= 'city'] input").setValue(validUser.getCity());
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(firstMeetingDate);
+        $("[data-test-id= 'name'] input").setValue(validUser.getName());
+        $("[data-test-id= 'phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id= 'agreement']").click();
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + firstMeetingDate), Duration.ofSeconds(20))
+                .shouldBe(Condition.visible);
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        $("[data-test-id= 'date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id= 'date'] input").setValue(secondMeetingDate);
+        $$("[type= 'button']").find(Condition.text("Запланировать")).click();
+        $("[data-test-id= 'replan-notification']")
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"), Duration.ofSeconds(20));
+        $$("[type= 'button']").filter(Condition.visible).first().click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + secondMeetingDate), Duration.ofSeconds(20))
+                .shouldBe(Condition.visible);
+    }
+}
